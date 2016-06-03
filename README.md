55100e595a5e08fca6718ea3ee8159e21a742539
Fannon <heimlersimon@gmail.com>
2016-06-03 15:17:21 +0200

    * MIT Licence * Docs

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# SemanticDummyEditor
-The SemanticDummyEditor extension for Semantic MediaWiki updates semantic property values that are dependent on another page's property values.
+# SemanticDependencyUpdater
+SemanticDependencyUpdater is a MediaWiki extension that monitors changes in the semantic data of a wiki page.
+The affected page can the Semantic Dependency Updater property to define which pages should also be updated.
+This can be defined through a list of pages or for more advanced use cases, a query string.
 
-See https://www.mediawiki.org/wiki/Extension:SemanticDummyEditor for more details.
+See https://www.mediawiki.org/wiki/Extension:SemanticDependencyUpdater for more details.
--- SemanticDependencyUpdater.php
+++ SemanticDependencyUpdater.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
- * SemanticDependencyUpdater is a MediaWiki extension that monitors changes in the semantic data of a wiki page
- * The affected page can the Semantic Dependency Updater property to define which pages should also be updated
+ * SemanticDependencyUpdater is a MediaWiki extension that monitors changes in the semantic data of a wiki page.
+ * The affected page can the Semantic Dependency Updater property to define which pages should also be updated.
  * This can be defined through a list of pages or for more advanced use cases, a query string.
  *
  * This extension requires Semantic MediaWiki >= 2.3 (http://www.semantic-mediawiki.org)
@@ -10,28 +10,6 @@
  * This extension is slightly based on Remco C. de Boers https://www.mediawiki.org/wiki/Extension:SemanticDummyEditor
  *
  * @author Simon Heimler, gesinn.it GmbH & Co
- *
- * MIT License
- *
- * Copyright (c) 2016, gesinn.it GmbH & Co
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
  */
 
 if (!defined('MEDIAWIKI')) {
