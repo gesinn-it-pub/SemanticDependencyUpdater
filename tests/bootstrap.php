cfb51dad829110617089f918267eed459bf9b122
Markus <92986684+gesinn-it-wam@users.noreply.github.com>
2022-07-08 13:48:48 +0200

    removed travis

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( PHP_SAPI !== 'cli' && PHP_SAPI !== 'phpdbg' ) {
-	die( 'Not an entry point' );
-}
-
-error_reporting( E_ALL | E_STRICT );
-date_default_timezone_set( 'UTC' );
-ini_set( 'display_errors', 1 );
